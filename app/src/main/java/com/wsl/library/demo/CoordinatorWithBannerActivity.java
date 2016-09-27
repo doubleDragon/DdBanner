9ae50efeb69a4a0c039b2699d060473ba8b895ec
doubleDragon <aiai373824745@gmail.com>
2016-09-27 20:39:55 +0800

    Add CoordinatorWithBannerActivity activity

--- app/src/main/java/com/wsl/library/demo/CoordinatorWithBannerActivity.java
+++ app/src/main/java/com/wsl/library/demo/CoordinatorWithBannerActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.wsl.library.banner.DdBannerIndicator;
 
@@ -20,6 +21,7 @@ public class CoordinatorWithBannerActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private BbAdapter adapter;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +31,14 @@ public class CoordinatorWithBannerActivity extends AppCompatActivity {
 
     private void initViews() {
         List<String> urls = new ArrayList<>();
-        urls.add("http://img5.imgtn.bdimg.com/it/u=1746418361,2823897370&fm=21&gp=0.jpg");
-        urls.add("http://img5.imgtn.bdimg.com/it/u=1479621666,13296461&fm=21&gp=0.jpg");
-        urls.add("http://img5.imgtn.bdimg.com/it/u=3614602665,2140950684&fm=21&gp=0.jpg");
-        urls.add("http://img1.imgtn.bdimg.com/it/u=2562925395,761784532&fm=21&gp=0.jpg");
+//        urls.add("http://img5.imgtn.bdimg.com/it/u=1746418361,2823897370&fm=21&gp=0.jpg");
+//        urls.add("http://img5.imgtn.bdimg.com/it/u=1479621666,13296461&fm=21&gp=0.jpg");
+//        urls.add("http://img5.imgtn.bdimg.com/it/u=3614602665,2140950684&fm=21&gp=0.jpg");
+//        urls.add("http://img1.imgtn.bdimg.com/it/u=2562925395,761784532&fm=21&gp=0.jpg");
+
+        urls.add("http://i4.piimg.com/567571/1cb06f22d0abc62c.png");
+        urls.add("http://i4.piimg.com/567571/1cb06f22d0abc62c.png");
+        urls.add("http://i4.piimg.com/567571/1cb06f22d0abc62c.png");
         adapter = new BbAdapter(this, urls);
 
         viewPager = (ViewPager) findViewById(R.id.dd_banner_viewpager);
@@ -46,6 +52,19 @@ public class CoordinatorWithBannerActivity extends AppCompatActivity {
             tabLayout.addTab(tabLayout.newTab().setText("aaa"));
             tabLayout.addTab(tabLayout.newTab().setText("bbb"));
             tabLayout.addTab(tabLayout.newTab().setText("ccc"));
+        }
+        View fab = findViewById(R.id.fab);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    List<String> urls = new ArrayList<>();
+                    urls.add("http://img5.imgtn.bdimg.com/it/u=1746418361,2823897370&fm=21&gp=0.jpg");
+                    urls.add("http://img5.imgtn.bdimg.com/it/u=1479621666,13296461&fm=21&gp=0.jpg");
+                    urls.add("http://img5.imgtn.bdimg.com/it/u=3614602665,2140950684&fm=21&gp=0.jpg");
+                    adapter.update(urls);
+                }
+            });
         }
     }
 }
